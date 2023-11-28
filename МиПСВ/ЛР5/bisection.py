317b2ae0a07da239ec11e0cd5f6163af60f887b4
IWDNO <zaharovpavel10133@gmail.com>
2023-11-28 12:37:30 +0300

    ЛР5 МиПСВ

--- МиПСВ/ЛР5/bisection.py
+++ МиПСВ/ЛР5/bisection.py
@@ -4,32 +4,56 @@ from typing import Tuple, Callable
 class Equation:
     @staticmethod
     def input_equation() -> Callable:
+        """ Ввод уравнения в общем виде """
+
+        # Степень уравнения
         n = int(input())
+        # Коэффициенты уравнения
         coefficients = [float(eval(x)) for x in input().split()]
+
+        # Создание строки, представляющей уравнение
         equation = "0"
         for i in range(n + 1):
             equation += f" + {coefficients[i]} * x**{n-i}"
-        
+
+        # Анонимная функция, представляющая введенное уравнение
         return lambda x: eval(equation)
+
 
     @staticmethod
     def bisection_method(func: Callable, a: float, b: float, epsilon: float) -> Tuple[int, float] | Tuple[None, None]:
+        """
+        Метод бисекции для нахождения корня уравнения.
+
+        func: Уравнение, для которого ищется корень\n
+        a: Левый конец интервала\n
+        b: Правый конец интервала\n
+        epsilon: Точность решения\n
+        return: Корень уравнения и количество итераций, необходимых для приближенного решения Или None, None, если метод не применим
+        """
+
+        # Проверка знаков на концах интервала
         if func(a) * func(b) > 0:
             print("Метод бисекции не применим. Начальные значения функции на концах интервала одного знака.")
             return None, None
-
+        
         iteration = 0
+
         while (b - a) > 2 * epsilon:
+            iteration += 1
             mid_point = (a + b) / 2
+            
+            # Если значение функции в середине интервала равно 0, возвращаем найденное значение
             if func(mid_point) == 0:
-                print(f"Найден корень: {mid_point}")
-                return mid_point
+                return mid_point, iteration
+            
+            # Иначе сдвигаем левую или правую границы
             elif func(mid_point) * func(a) < 0:
                 b = mid_point
             else:
                 a = mid_point
-            iteration += 1
 
+        # Найденный корень
         root = (a + b) / 2
 
         return root, iteration
--- МиПСВ/ЛР5/newton.py
+++ МиПСВ/ЛР5/newton.py
@@ -5,14 +5,24 @@ from typing import Callable, Tuple
 class Equation:
     @staticmethod
     def equation_input():
+        """ Ввод уравнения пользователем """
         equation_str = input()
         return sympify(equation_str)
 
     @staticmethod
     def newton_method(f: Callable, df: Callable, initial_guess: float, e: float) -> Tuple[float | int]:
+        """
+        Метод Ньютона для нахождения корня уравнения.
+
+        f: Функция уравнения\n
+        df: Производная функции уравнения\n
+        initial_guess: Начальное приближение\n
+        e: Точность решения\n
+        return: Корень уравнения и количество итераций, необходимых для приближенного решения
+        """
         x = initial_guess
         new_x = x - f(x) / df(x)
-        iteration = 0
+        iteration = 1
         while abs(new_x - x) > e:
             x = new_x
             new_x = new_x - f(new_x) / df(new_x)
@@ -22,15 +32,50 @@ class Equation:
     
     @staticmethod
     def simple_newton_method(f: Callable, df: Callable, initial_guess: float, e: float) -> Tuple[float | int]:
+        """
+        Упрощенный метод Ньютона для нахождения корня уравнения.
+
+        f: Функция уравнения\n
+        df: Производная функции уравнения (первоначальная)\n
+        initial_guess: Начальное приближение\n
+        e: Точность решения\n
+        return: Корень уравнения и количество итераций, необходимых для приближенного решения
+        """
         x = initial_guess
+
+        # Значение производной, найденное в начальной точке
         df0 = df(x)
+        
         new_x = x - f(x) / df0
-        iteration = 0
+        iteration = 1
         while abs(new_x - x) > e:
             x = new_x
             new_x = new_x - f(new_x) / df0
             iteration += 1
 
+        return new_x, iteration
+    
+    @staticmethod
+    def secant_method(f: Callable, initial_guess: float, e: float) -> Tuple[float | int]:
+        """
+        Метод секущих для нахождения корня уравнения.
+
+        f: Функция уравнения\n
+        initial_guess: Начальное приближение\n
+        e: Точность решения\n
+        return: Корень уравнения и количество итераций, необходимых для приближенного решения
+        """
+        x = initial_guess
+        # Первое приближение
+        new_x = x - (f(x) * e) / (f(x + e) - f(x))
+        iteration = 1
+        # Все последующие
+        while abs(new_x - x) > e:
+            tmp_x = new_x
+            new_x = new_x - (f(new_x)*(x - new_x)) / (f(x) - f(new_x))
+            x = tmp_x
+            iteration += 1
+        
         return new_x, iteration
     
 
@@ -47,9 +92,9 @@ def main():
 
     for guess in initial_guesses:
         print(Equation.newton_method(f, df, guess, e))
+        # print(Equation.simple_newton_method(f, df, guess, e))
+        # print(Equation.secant_method(f, guess, e))
     
-    # for guess in initial_guesses:
-    #     print(Equation.simple_newton_method(f, df, guess, e))
 
 if __name__ == "__main__":
     main()
